aa8a52a689be2df5401efe2d2ce5829c637a1115
Gopal-G0 <heyitsmegopal@gmail.com>
2025-05-22 11:23:29 +0530

    Solved LeetCode -344. Reverse String

---
+++ Strings/Two Pointers/reverseString.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseString(string &str) {
+
+    int s = 0;
+    int e = str.size() - 1;
+
+    while (s < e)
+    {
+        swap(str[s++], str[e--]);
+    }
+}
+
+int main() {
+
+    string str;
+    cout << "Enter String to reverse : ";
+    cin >> str;
+
+    reverseString(str);
+    cout << "Reversed String : " << str;
+    return 0;
+}
