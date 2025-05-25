7ed56f8b02cde1157e37425a0643ff4f5e1faa86
Gopal-G0 <heyitsmegopal@gmail.com>
2025-05-25 20:52:04 +0530

    Solved LeetCode - Problem.1394

---
+++ Arrays/HashTable/findLuckyInteger.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+int findLuckyInteger(vector<int>& arr) {
+    int largestLuckyInt = -1;
+    int luckyInt = 0;
+    unordered_map<int, int> freqMap;
+
+    for (int num : arr) {
+        freqMap[num]++;
+    }
+
+    for (const auto& pair : freqMap) {
+        if (pair.first == pair.second) {
+            luckyInt = pair.first;
+
+            if (luckyInt > largestLuckyInt) {
+                largestLuckyInt = luckyInt;
+            }
+        }
+    }
+
+    return largestLuckyInt;
+}
+
+int main() {
+    vector<int> arr = {1,2,2,3,3,3,4,4,4,4,5,6};
+    int largestLuckyInt = findLuckyInteger(arr);
+
+    cout << "Largest Lucky Number : " << largestLuckyInt << endl;
+
+    return 0;
+}
